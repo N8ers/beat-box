ade522476fe1760465494de60742f341d5b8ee39
Nathan <nathanraysheryak@gmail.com>
2021-05-30 07:38:00 -0400

    set grid values in state + toggleable

--- src/BeatGrid.js
+++ src/BeatGrid.js
@@ -1,37 +1,90 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
 
-function BeatGrid(props) {
-  let measures = [1, 2, 3, 4];
-  let liteGrid = [
-    { kick: [1,0,1,0] },
-    { snare: [0,1,0,1] },
-    { hh: [1,1,1,1] },
-  ]
+class BeatGrid extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      measures: [1, 2, 3, 4],
+      liteGrid: {
+        kick: [1, 0, 1, 0],
+        snare: [0, 1, 0, 1],
+        hh: [1, 1, 1, 1],
+      },
+    };
+  }
 
-  return (
-    <div className="w250">
-      <Grid container spacing={0}>
-        <Grid>
-          <Grid item>kick</Grid>
-          <Grid item>snare</Grid>
-          <Grid item>hh</Grid>
-        </Grid>
+  toggleLite = (beat, kitPiece) => {
+    let newGrid = this.state.liteGrid;
+    let newValue = newGrid[kitPiece][beat - 1] === 0 ? 1 : 0;
+    newGrid[kitPiece][beat - 1] = newValue;
 
-        {measures.map((beat) => {
-          return (
-            <Grid key={"beat" + beat} id={"beat" + beat} container item xs={2}>
-              <Grid className={props.beat === beat ? "lite" : ""}>
-                <Grid item>[]</Grid>
-                <Grid item>[]</Grid>
-                <Grid item>[]</Grid>
+    this.setState({
+      liteGrid: newGrid,
+    });
+  };
+
+  render() {
+    return (
+      <div className="w250">
+        <Grid container spacing={0}>
+          <Grid>
+            <Grid item>kick</Grid>
+            <Grid item>snare</Grid>
+            <Grid item>hh</Grid>
+          </Grid>
+
+          {this.state.measures.map((beat) => {
+            return (
+              <Grid
+                key={"beat" + beat}
+                id={"beat" + beat}
+                container
+                item
+                xs={2}
+              >
+                <Grid className={this.props.beat === beat ? "lite" : ""}>
+                  <Grid
+                    item
+                    onClick={() => this.toggleLite(beat, "kick")}
+                    className={
+                      this.state.liteGrid["kick"][beat - 1] === 1
+                        ? "selected-box"
+                        : ""
+                    }
+                  >
+                    []
+                  </Grid>
+                  <Grid
+                    item
+                    onClick={() => this.toggleLite(beat, "snare")}
+                    className={
+                      this.state.liteGrid["snare"][beat - 1] === 1
+                        ? "selected-box"
+                        : ""
+                    }
+                  >
+                    []
+                  </Grid>
+                  <Grid
+                    item
+                    onClick={() => this.toggleLite(beat, "hh")}
+                    className={
+                      this.state.liteGrid["hh"][beat - 1] === 1
+                        ? "selected-box"
+                        : ""
+                    }
+                  >
+                    []
+                  </Grid>
+                </Grid>
               </Grid>
-            </Grid>
-          );
-        })}
-      </Grid>
-    </div>
-  );
+            );
+          })}
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default BeatGrid;
